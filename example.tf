491dca8fa06df339aaab88274ce49575578f3b45
Mike Schwörer <mailport@mikescher.de>
2017-03-20 20:03:55 +0100

    Update syntax file

--- example.tf
+++ example.tf
@@ -4,8 +4,29 @@
 */
 
 program example : display[0, 0]
+	var stack<int>[12] buffer;
 	begin
-		out "Hello World\r\n";
+		out buffer.count();
+		buffer.push(four());
+		out buffer.count();
+		buffer.push(four() + four());
+		out buffer.count();
+		buffer.push(four() * four() - 1);
+		out buffer.count();
+		buffer.push(four() * four());
+		out buffer.count();
+		buffer.push(four()*4+3);
+		out buffer.count();
+		buffer.push(10*four() + 2);
+		out buffer.count();
+
+
+
 		quit;
+	end
+	 
+	int four()
+	begin
+	 return 4;
 	end
 end
